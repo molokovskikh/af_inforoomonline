c9d803c795bff57914b67acf98890beddea3b760
Kvasov <Kvasov@e393182f-8c28-f24b-a96b-f6e314bb1ab7>
2008-04-18 14:22:00 +0000

    Об новленные тесты

--- src/InforoomOnline.Tests/InforoomOnlineServiceFixture.cs
+++ src/InforoomOnline.Tests/InforoomOnlineServiceFixture.cs
@@ -1,66 +1,66 @@
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using NUnit.Framework;
 
 namespace InforoomOnline.Tests
 {
-	[TestFixture]
-	public class InforoomOnlineServiceFixture
-	{
-		[Test]
-		public void GetNameFromCatalog()
-		{
-			InforoomOnlineService service = new InforoomOnlineService();
-			LogDataSet(service.GetNamesFromCatalog(new string[0], new string[0], false, 100, 0));
-			LogDataSet(service.GetNamesFromCatalog(new string[] { "*Тест*" }, new string[0], false, 100, 0));
-			LogDataSet(service.GetNamesFromCatalog(new string[0], new string[] { "*Тест*" }, false, 100, 0));
-			LogDataSet(service.GetNamesFromCatalog(new string[0], new string[0], true, 100, 0));
-			LogDataSet(service.GetNamesFromCatalog(new string[] { "*Тест*" }, new string[0], true, 100, 0));
-			LogDataSet(service.GetNamesFromCatalog(new string[0], new string[] { "*Тест*" }, true, 100, 0));
-		}
+    [TestFixture]
+    public class InforoomOnlineServiceFixture
+    {
+        [Test]
+        public void GetNameFromCatalog()
+        {
+            var service = new InforoomOnlineService();
+            LogDataSet(service.GetNamesFromCatalog(new string[0], new string[0], false, 100, 0));
+            LogDataSet(service.GetNamesFromCatalog(new[] {"*Тест*"}, new string[0], false, 100, 0));
+            LogDataSet(service.GetNamesFromCatalog(new string[0], new[] {"*Тест*"}, false, 100, 0));
+            LogDataSet(service.GetNamesFromCatalog(new string[0], new string[0], true, 100, 0));
+            LogDataSet(service.GetNamesFromCatalog(new[] {"*Тест*"}, new string[0], true, 100, 0));
+            LogDataSet(service.GetNamesFromCatalog(new string[0], new[] {"*Тест*"}, true, 100, 0));
+        }
 
-		[Test]
-		public void GetOffers()
-		{
-			InforoomOnlineService service = new InforoomOnlineService();
-			DataSet data = service.GetOffers(new string[] { "PriceCode", "Code", "FullCode" }, new string[] { "32", "ОФ-659", "1" }, false, new string[0], new string[0], 100, 0);
-		}
+        [Test]
+        public void GetOffers()
+        {
+            var service = new InforoomOnlineService();
+            var data = service.GetOffers(null, null, false,
+                                             new string[0], new string[0], 100, 0);
+            LogDataSet(data);
+        }
 
-		[Test]
-		public void GetPriceList()
-		{
-			InforoomOnlineService service = new InforoomOnlineService();
-			LogDataSet(service.GetPriceList(new string[0]));
-			LogDataSet(service.GetPriceList(new string[] {"%а%"}));
-		}
+        [Test]
+        public void GetPriceList()
+        {
+            var service = new InforoomOnlineService();
+            LogDataSet(service.GetPriceList(new string[0]));
+            LogDataSet(service.GetPriceList(new[] {"%а%"}));
+        }
 
-		[Test]
-		public void PostOrder()
-		{
-			InforoomOnlineService service = new InforoomOnlineService();
-			service.PostOrder(new long[] { 687471520 }, new int[] { 1 }, new string[] { "это тестовый заказ" });
-		}
+        [Test]
+        public void PostOrder()
+        {
+            var service = new InforoomOnlineService();
+            service.PostOrder(new long[] {687471520}, new[] {1}, new[] {"это тестовый заказ"});
+        }
 
-		private static void LogDataSet(DataSet dataSet)
-		{
-			foreach (DataTable dataTable in dataSet.Tables)
-			{
-				Console.WriteLine("<table>");
-				foreach (DataRow dataRow in dataTable.Rows)
-				{
-					Console.WriteLine("\t<row>");
-					Console.Write("\t");
-					foreach (DataColumn column in dataTable.Columns)
-					{
-						Console.Write(dataRow[column] + " ");
-					}
-					Console.WriteLine();
-					Console.WriteLine("\t</row>");
-				}
-				Console.WriteLine("</table>");
-			}
-		}
-	}
+        private static void LogDataSet(DataSet dataSet)
+        {
+            foreach (DataTable dataTable in dataSet.Tables)
+            {
+                Console.WriteLine("<table>");
+                foreach (DataRow dataRow in dataTable.Rows)
+                {
+                    Console.WriteLine("\t<row>");
+                    Console.Write("\t");
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        Console.Write(dataRow[column] + " ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("\t</row>");
+                }
+                Console.WriteLine("</table>");
+            }
+        }
+    }
 }
---
+++ src/InforoomOnline.Tests/IntegrationFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace InforoomOnline.Tests
+{
+    [TestFixture]
+    public class IntegrationFixture
+    {
+        [Test]
+        public void IntegrationTest()
+        {
+            var service = new localhost.InforoomOnlineService();
+            var offers = service.GetOffers(null, null, false, true, null, null, 1000, true, 0, true);
+            Assert.That(offers.Tables.Count,
+                        Is.EqualTo(1));
+        }
+    }
+}
