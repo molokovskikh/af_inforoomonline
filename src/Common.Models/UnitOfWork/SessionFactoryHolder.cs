742d767e234f9644fa81308a491c9d08b0960d83
kvasov <kvasov@e393182f-8c28-f24b-a96b-f6e314bb1ab7>
2008-12-12 10:15:03 +0000

    Добавил ленивою инициализацию SessionFactoryHolder

--- src/Common.Models/UnitOfWork/SessionFactoryHolder.cs
+++ src/Common.Models/UnitOfWork/SessionFactoryHolder.cs
@@ -6,22 +6,29 @@ namespace Common.Models
     public class SessionFactoryHolder : ISessionFactoryHolder
     {
 	    private readonly Configuration _configuration;
+    	private ISessionFactory _sessionFactory;
+		private readonly object _sessionFactoryLock = new object();
 
 	    public SessionFactoryHolder()
 		{
 			_configuration = new Configuration();
 		}
 
-        public void BuildSessionFactory()
-        {
-            SessionFactory = _configuration.BuildSessionFactory();
-        }
-
 	    public Configuration Configuration
 	    {
             get { return _configuration; }
 	    }
 
-        public ISessionFactory SessionFactory{ get; private set; }
+		public ISessionFactory SessionFactory
+		{
+			get
+			{
+				if (_sessionFactory == null)
+					lock (_sessionFactoryLock)
+						if (_sessionFactory == null)
+							_sessionFactory = _configuration.BuildSessionFactory();
+				return _sessionFactory;
+			}
+		}
 	}
 }
