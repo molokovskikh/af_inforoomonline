075df4f90b9e8facc8e2735ab287a6d6b87c6cf3
Квасов Роман <r.kvasov@analit.net>
2013-12-30 14:27:42 +0400

    Исправил запуск тестов

--- src/InforoomOnline.Tests/FixtureSetup.cs
+++ src/InforoomOnline.Tests/FixtureSetup.cs
@@ -1,14 +1,27 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using CassiniDev;
+using NUnit.Framework;
 
 namespace InforoomOnline.Tests
 {
 	[SetUpFixture]
 	public class FixtureSetup
 	{
+		private Server server;
+
 		[SetUp]
 		public void Setup()
 		{
+			server = new Server(54860, Path.GetFullPath(@"..\..\..\InforoomOnline"));
+			server.Start();
 			Test.Support.Setup.Initialize("Main");
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			if (server != null)
+				server.Dispose();
 		}
 	}
 }
